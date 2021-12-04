17aef68da4c099d2d00d7c61123ffc2ccda334ca
Matthew Slane <matthew.slane1@gmail.com>
2021-12-04 11:28:30 -0800

    day one part two

--- day1.py
+++ day1.py
@@ -12,7 +12,23 @@ def part_one(depths: List[int]) -> int:
 			result += 1
 	return result
 
+def part_two(depths: List[int]) -> int:
+	if len(depths) <= 3:
+		return 0
+	result = 0
+
+	prev_depth = depths[0] + depths[1] + depths[2]
+	for i in range(3, len(depths)):
+		new_depth = prev_depth - depths[i - 3] + depths[i]
+		if new_depth > prev_depth:
+			result += 1
+		prev_depth = new_depth
+
+	return result
+
 if __name__ == '__main__':
 	depths = read_input()
 	result_one = part_one(depths)
 	print(result_one)
+	result_two = part_two(depths)
+	print(result_two)
