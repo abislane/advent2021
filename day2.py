2c1aa93cb0ded496c7bb253959fa5b4809e730af
Matthew Slane <matthew.slane1@gmail.com>
2021-12-04 11:43:19 -0800

    day 2 part 2

--- day2.py
+++ day2.py
@@ -27,7 +27,23 @@ def part_one(steps: List[Step]) -> int:
 			depth -= step.magnitude
 	return horiz * depth
 
+def part_two(steps: List[Step]) -> int:
+	horiz = 0
+	depth = 0
+	aim = 0
+	for step in steps:
+		if step.direction == 'forward':
+			horiz += step.magnitude
+			depth += step.magnitude * aim
+		elif step.direction == 'down':
+			aim += step.magnitude
+		elif step.direction == 'up':
+			aim -= step.magnitude
+	return horiz * depth
+
 if __name__ == '__main__':
 	steps = read_input()
 	result_one = part_one(steps)
 	print(result_one)
+	result_two = part_two(steps)
+	print(result_two)
